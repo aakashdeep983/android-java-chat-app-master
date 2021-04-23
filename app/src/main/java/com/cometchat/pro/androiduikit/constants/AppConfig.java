c8f4a93ce109a53e2521a3cf5dd14594ba5f7778
AAKASH DEEP <48329027+aakashdeep983@users.noreply.github.com>
2021-04-23 15:45:06 +0530

    Initial commit

---
+++ app/src/main/java/com/cometchat/pro/androiduikit/constants/AppConfig.java
@@ -0,0 +1,14 @@
+package com.cometchat.pro.androiduikit.constants;
+
+public class AppConfig {
+
+    public class AppDetails {
+
+        public static final String APP_ID = "XXXXXXXXXXXXXXXXX";
+
+        public static final String AUTH_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+        public static final String REGION = "XX";
+    }
+}
+
